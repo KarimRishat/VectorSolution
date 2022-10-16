217264c38a792ed7cd8efa8e014608dbcf677cb8
Rishat <karimr2000@mail.ru>
2022-10-17 01:13:12 +0300

    1

--- VectorProject/StreamOutput.cpp
+++ VectorProject/StreamOutput.cpp
@@ -1,10 +1,10 @@
 #include "StreamOutput.h"
 template <typename T>
-std::ostream& operator<<(std::ostream& o, const VectorSpace::Vector<T> v)
+inline std::ostream& operator<<(std::ostream& o, const VectorSpace::Vector<T> v)
 {
 	for (size_t i = 0; i < v.size(); i++)
 	{
-		o << v[i];
+		o << v[i] << " ";
 	}
 	return o;
 }
